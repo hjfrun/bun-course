e1f678b62e2ae880343efdc0f808901a833472c8
hjfrun <hjfrun@gmail.com>
2024-04-12 22:32:53 +0800

    02 web server & multi route

--- index.ts
+++ index.ts
@@ -1,8 +1,24 @@
-const z = 10
+import figlet from "figlet"
 
-const read = (text: string) => {
-  return text
-}
+const server = Bun.serve({
+  port: 3000,
+  fetch(req) {
+    const url = new URL(req.url)
+    if (url.pathname === '/') {
+      const body = figlet.textSync('Hello LinTing, I am learning Bun!')
+      return new Response(body)
+    }
 
-const message = read('Hello World!@')
-console.log(message)
+    if (url.pathname === '/about') {
+      return new Response('About page')
+    }
+
+    if (url.pathname === '/contact') {
+      return new Response('Contact page')
+    }
+
+    return new Response('Not Found', { status: 404 })
+  }
+})
+
+console.log(`Server running at http://localhost:${server.port}`)
